9ad564799a1704ada13b608fc12c89c44ff3c2b0
Christine <yammy0623@gmail.com>
2025-06-03 19:34:07 +0800

    add: test for multiple r

---
+++ demo_test.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+r=10
+echo "Running test with --r=$r"
+CONFIG="configs/diff_r_test/tome-vit-adapter-t-3x_$r.py"
+echo "config $CONFIG"
+python selector_demo.py data/coco/val2017/000000046252.jpg $CONFIG pretrained/vit-adapter-t-3x.pth $r
+
+
+for r in $(seq 100 100 1000); do
+    echo "Running test with --r=$r"
+    CONFIG="configs/diff_r_test/tome-vit-adapter-t-3x_$r.py"
+    echo "config $CONFIG"
+    python selector_demo.py data/coco/val2017/000000046252.jpg $CONFIG pretrained/vit-adapter-t-3x.pth $r
+done
